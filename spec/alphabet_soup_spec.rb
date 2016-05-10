f2a7ece41aeb138b8875c9621c857c445bed05c7
snlamm <shmuel.lamm@gmail.com>
2016-05-10 15:30:37 -0400

    add rspec tests

--- spec/alphabet_soup_spec.rb
+++ spec/alphabet_soup_spec.rb
@@ -8,4 +8,33 @@ describe AlphabetSoup do
   it 'does something useful' do
     expect(true).to eq(true)
   end
+  it 'creates a new Alphabetize instance ' do 
+    x = Alphabetize.new
+    expect(x).to be_a(Alphabetize)
+  end
+
+  it '#alphabet creates a new alphabet' do 
+    x = Alphabetize.new
+    x.alphabet = ".xyzabc"
+    expect(x.alphabet).to eq(".xyzabc")
+  end
+
+  it '#alphabetize sorts according to the alphabet' do
+    x = Alphabetize.new
+    x.alphabet = ".xyzabc"
+    arr = ["cab", "bay", "baz.", ".baz"]
+    arr_sorted = [".baz", "bay", "baz.", "cab"]
+    sorted_alphabet = x.alphabetize(arr)
+    expect(sorted_alphabet).to eq(arr_sorted)
+  end
+
+  it '#alphabetize sorts array with letters not in alphabet' do 
+    x = Alphabetize.new
+    x.alphabet = ".xyzabc"
+    arr = ["cab", "bay", ".baz", "..baz", ".d.baz"]
+    arr_sorted = ["..baz", ".baz", ".d.baz", "bay", "cab"]
+    sorted_alphabet = x.alphabetize(arr)
+    expect(sorted_alphabet).to eq(arr_sorted)
+  end
+
 end
