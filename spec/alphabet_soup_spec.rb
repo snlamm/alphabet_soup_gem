993b9d46d03aa291d5b210a63b65a015ff73b37e
snlamm <shmuel.lamm@gmail.com>
2016-05-10 15:48:16 -0400

    add before block to rspec tests

--- spec/alphabet_soup_spec.rb
+++ spec/alphabet_soup_spec.rb
@@ -5,35 +5,34 @@ describe AlphabetSoup do
     expect(AlphabetSoup::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(true).to eq(true)
+  #it 'does something useful' do
+  #  expect(true).to eq(true)
+  #end
+  before(:example) do 
+    @x = Alphabetize.new
+    @x.alphabet = ".xyzabc"
   end
+
   it 'creates a new Alphabetize instance ' do 
-    x = Alphabetize.new
-    expect(x).to be_a(Alphabetize)
+    #x = Alphabetize.new
+    expect(@x).to be_a(Alphabetize)
   end
 
   it '#alphabet creates a new alphabet' do 
-    x = Alphabetize.new
-    x.alphabet = ".xyzabc"
-    expect(x.alphabet).to eq(".xyzabc")
+    expect(@x.alphabet).to eq(".xyzabc")
   end
 
   it '#alphabetize sorts according to the alphabet' do
-    x = Alphabetize.new
-    x.alphabet = ".xyzabc"
     arr = ["cab", "bay", "baz.", ".baz"]
     arr_sorted = [".baz", "bay", "baz.", "cab"]
-    sorted_alphabet = x.alphabetize(arr)
+    sorted_alphabet = @x.alphabetize(arr)
     expect(sorted_alphabet).to eq(arr_sorted)
   end
 
   it '#alphabetize sorts array with letters not in alphabet' do 
-    x = Alphabetize.new
-    x.alphabet = ".xyzabc"
     arr = ["cab", "bay", ".baz", "..baz", ".d.baz"]
     arr_sorted = ["..baz", ".baz", ".d.baz", "bay", "cab"]
-    sorted_alphabet = x.alphabetize(arr)
+    sorted_alphabet = @x.alphabetize(arr)
     expect(sorted_alphabet).to eq(arr_sorted)
   end
 
